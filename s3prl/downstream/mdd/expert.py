1ff64df1e3e37a9e371be0f4df506fa9426d8b9e
Kornel Szabo <kgs37@cam.ac.uk>
2023-12-12 10:50:06 +0000

    Relax assumption on the canonical and perceived phone alignment. We can now use any true canonical and perceived sequences.

--- s3prl/downstream/mdd/expert.py
+++ s3prl/downstream/mdd/expert.py
@@ -37,15 +37,11 @@ from speechbrain.utils.edit_distance import (
     wer_details_by_utterance,
 )
 
-# from pyctcdecode import build_ctcdecoder
-
 # -------------#
 from .model import Model
 from .dataset import L2ArcticDataset
 
 
-# This is just copied from the TIMIT phone recognition task
-# I haven't understood what the things do just yet
 class DownstreamExpert(nn.Module):
     """
     Used to handle downstream-specific operations
@@ -83,7 +79,7 @@ class DownstreamExpert(nn.Module):
             self.arpa_phone_list = [phone for (i, phone) in arpa_tuples]
 
         # Blank is the empty label. In this case, it is the label of silence
-        self.objective = nn.CTCLoss(blank=self.arpa_phones["sil"], reduction="none")
+        self.objective = nn.CTCLoss(blank=self.arpa_phones["sil"], reduction="mean")
 
         self.logging = Path(expdir) / "log.log"
         self.best = defaultdict(lambda: 0)
@@ -121,6 +117,271 @@ class DownstreamExpert(nn.Module):
             collate_fn=self.dataset[split].collate_fn,
         )
 
+    def compute_alignment(self, reference, hypothetical, empty_token=-1):
+        # TODO: make faster
+        dp = [
+            [0 for j in range(len(hypothetical) + 1)] for i in range(len(reference) + 1)
+        ]
+        par = [
+            [(-1, -1) for j in range(len(hypothetical) + 1)]
+            for i in range(len(reference) + 1)
+        ]
+
+        # Initialization of sides of the table
+        for i in range(1, len(dp)):
+            dp[i][0] = i
+            par[i][0] = (i - 1, 0)
+        for j in range(1, len(dp[0])):
+            dp[0][j] = j
+            par[0][j] = (0, j - 1)
+
+        # Computing best path
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                dp[i][j] = 1e9
+                # Try eq or sub if possible
+                if (
+                    dp[i - 1][j - 1] + (reference[i - 1] != hypothetical[j - 1])
+                    < dp[i][j]
+                ):
+                    dp[i][j] = dp[i - 1][j - 1] + (
+                        reference[i - 1] != hypothetical[j - 1]
+                    )
+                    par[i][j] = (i - 1, j - 1)
+                # If I or D is better, do that
+                if dp[i][j - 1] + 1 < dp[i][j]:
+                    dp[i][j] = dp[i][j - 1] + 1
+                    par[i][j] = (i, j - 1)
+                if dp[i - 1][j] + 1 < dp[i][j]:
+                    dp[i][j] = dp[i - 1][j] + 1
+                    par[i][j] = (i - 1, j)
+
+        # Retracing path
+        path = []
+        reference_aligned = []
+        hypothetical_aligned = []
+        reference_idx = len(reference)
+        hypothetical_idx = len(hypothetical)
+
+        while reference_idx > 0 or hypothetical_idx > 0:
+            par_reference, par_hypothetical = par[reference_idx][hypothetical_idx]
+
+            op = None
+
+            if (reference_idx - 1, hypothetical_idx - 1) == (
+                par_reference,
+                par_hypothetical,
+            ):
+                op = None
+                if reference[reference_idx - 1] == hypothetical[hypothetical_idx - 1]:
+                    op = "="
+                else:
+                    op = "S"
+                path.append((op, reference_idx - 1, hypothetical_idx - 1))
+                reference_aligned.append(reference[reference_idx - 1])
+                hypothetical_aligned.append(hypothetical[hypothetical_idx - 1])
+            elif (reference_idx - 1, hypothetical_idx) == (
+                par_reference,
+                par_hypothetical,
+            ):
+                path.append(("I", reference_idx - 1, None))
+                reference_aligned.append(reference[reference_idx - 1])
+                hypothetical_aligned.append(empty_token)
+            elif (reference_idx, hypothetical_idx - 1) == (
+                par_reference,
+                par_hypothetical,
+            ):
+                path.append(("D", None, hypothetical_idx - 1))
+                reference_aligned.append(empty_token)
+                hypothetical_aligned.append(hypothetical[hypothetical_idx - 1])
+            else:
+                raise IndexError(
+                    f"Invalid parents for index ({reference_idx}, {hypothetical_idx}): ({par_reference}, {par_hypothetical})"
+                )
+
+            reference_idx, hypothetical_idx = par_reference, par_hypothetical
+
+        path = list(reversed(path))
+        reference_aligned = list(reversed(reference_aligned))
+        hypothetical_aligned = list(reversed(hypothetical_aligned))
+
+        assert len(reference_aligned) == len(
+            hypothetical_aligned
+        ), f"""Unequal lengths detected for the aligned sequences
+reference: {len(reference_aligned)}
+hypothetical: {len(hypothetical_aligned)}
+"""
+
+        return {
+            "edit_distance": dp[-1][-1],
+            "alignment": path,
+            "reference_aligned": reference_aligned,
+            "hypothetical_aligned": hypothetical_aligned,
+        }
+
+    def compute_asr_metrics(
+        self,
+        true_canonical_phones: torch.LongTensor,
+        pred_canonical_phones: torch.LongTensor,
+    ):
+        """
+        Computes the ASR metrics for the phone reconstruction of our model.
+
+        @returns: collections.Counter
+            The updated running statistics, with keys:
+
+            "WER" - word error rate
+            "insertions" - number of insertions
+            "deletions" - number of deletions
+            "substitutions" - number of substitutions
+            "num_ref_tokens" - number of reference tokens
+        """
+        counter = Counter()
+        prediction_error_metrics = accumulatable_wer_stats(
+            refs=true_canonical_phones,
+            hyps=pred_canonical_phones,
+            stats=counter,
+        )
+        return prediction_error_metrics
+
+    def compute_mdd_metrics(
+        self,
+        true_perceived_phones: torch.LongTensor,
+        true_canonical_phones: torch.LongTensor,
+        pred_canonical_phones: torch.LongTensor,
+    ):
+        """
+        TODO: this only works for a single batch
+        TODO: make faster
+
+        Computes precision, recall and F1-score for the mispronunciation detection model.
+        All of these need the following 4 metrics:
+        - number of true positives
+        - number of true negatives
+        - number of false positives
+        - number of false negatives
+
+        We compute these as follows:
+        1. We align the sequences (using edit-distance alignment) in the following ways:
+            - align true perceieved and true canonical phones (true alignment)
+            - align true perceieved and predicted canonical phones (pred alignment)
+            - pad all of the alignments with empty "<eps>" characters such that all three strings are aligned
+        2. Using the alignments, we compute the errors:
+            - true positives: neither the true or the predicted canonical phone agree with the true perceived phone
+            - true negatives: both the true and the predicted canonical phone agree with the true perceived phone
+            - false positives: the true canonical and true perceived phones are the same, but the predicted canonical phone differs
+            - false negatives: the predicted canonical and true perceived phones are the same, but the true canonical phone differs
+        """
+        empty_token = -1
+        true_alignment = self.compute_alignment(
+            reference=true_perceived_phones,
+            hypothetical=true_canonical_phones,
+            empty_token=empty_token,
+        )
+
+        pred_alignment = self.compute_alignment(
+            reference=true_perceived_phones,
+            hypothetical=pred_canonical_phones,
+            empty_token=empty_token,
+        )
+
+        # Pad the alignments with empty characters so that they are all aligned
+        aligned_true_canonical_phones = []
+        aligned_true_perceived_phones = []
+        aligned_pred_canonical_phones = []
+
+        true_alignment_idx = 0
+        pred_alignment_idx = 0
+
+        true_reference = true_alignment["reference_aligned"]
+        pred_reference = pred_alignment["reference_aligned"]
+        true_hypothetical = true_alignment["hypothetical_aligned"]
+        pred_hypothetical = pred_alignment["hypothetical_aligned"]
+        while true_alignment_idx < len(true_reference) and pred_alignment_idx < len(
+            pred_reference
+        ):
+            if true_reference[true_alignment_idx] == pred_reference[pred_alignment_idx]:
+                aligned_true_canonical_phones.append(
+                    true_hypothetical[true_alignment_idx]
+                )
+                aligned_pred_canonical_phones.append(
+                    pred_hypothetical[pred_alignment_idx]
+                )
+                aligned_true_perceived_phones.append(true_reference[true_alignment_idx])
+                true_alignment_idx += 1
+                pred_alignment_idx += 1
+            elif true_reference[true_alignment_idx] == empty_token:
+                aligned_true_canonical_phones.append(
+                    true_hypothetical[true_alignment_idx]
+                )
+                aligned_pred_canonical_phones.append(empty_token)
+                aligned_true_perceived_phones.append(true_reference[true_alignment_idx])
+                true_alignment_idx += 1
+            elif pred_reference[pred_alignment_idx] == empty_token:
+                aligned_true_canonical_phones.append(empty_token)
+                aligned_pred_canonical_phones.append(
+                    pred_hypothetical[pred_alignment_idx]
+                )
+                aligned_true_perceived_phones.append(true_reference[true_alignment_idx])
+                pred_alignment_idx += 1
+            else:
+                assert False, f"""Invalid alignment parse for strings:
+true_reference: {true_reference}
+pred_reference: {pred_reference}
+true_hypothetical: {true_hypothetical}
+pred_hypothetical: {pred_hypothetical}
+
+Exiting...
+"""
+
+        tp = 0
+        fp = 0
+        tn = 0
+        fn = 0
+        corr_diag=0
+        err_diag=0
+
+        for true_canonical_phone, pred_canonical_phone, true_perceived_phone in zip(
+            aligned_true_canonical_phones,
+            aligned_pred_canonical_phones,
+            aligned_true_perceived_phones,
+        ):
+            if true_canonical_phone != true_perceived_phone:
+                if pred_canonical_phone != true_perceived_phone:
+                    tp += 1
+                    if pred_canonical_phone == true_canonical_phone:
+                        corr_diag += 1
+                    else:
+                        err_diag += 1
+                else:
+                    fn += 1
+            else:
+                if pred_canonical_phone != true_perceived_phone:
+                    fp += 1
+                else:
+                    tn += 1
+
+        precision = 0
+        if tp + fp > 0:
+            precision = tp / (tp + fp)
+        recall = 0
+        if tp + fn > 0:
+            recall = tp / (tp + fn)
+        f1_score = 0
+        if precision != 0 or recall != 0:
+            f1_score = 2 / ((1 / precision) + (1 / recall))
+
+        corr_diag_pct = corr_diag / tp if tp > 0 else 0
+        err_diag_pct = err_diag / tp if tp > 0 else 0
+
+        return {
+            "corr_diag_pct": corr_diag_pct,
+            "err_diag_pct": err_diag_pct,
+            "precision": precision,
+            "recall": recall,
+            "f1_score": f1_score,
+        }
+
     # Interface
     """runner.py releavant code:
         loss = self.downstream.model(
@@ -134,8 +395,8 @@ class DownstreamExpert(nn.Module):
         self,
         split: str,
         features: List[torch.FloatTensor],
-        true_perceived_phones: torch.FloatTensor,
-        true_canonical_phones: torch.FloatTensor,
+        true_perceived_phones: torch.LongTensor,
+        true_canonical_phones: torch.LongTensor,
         perceived_lengths: torch.LongTensor,
         canonical_lengths: torch.LongTensor,
         records: Dict[str, List[any]],
@@ -224,127 +485,31 @@ class DownstreamExpert(nn.Module):
         # TODO: check if there is a way to store the stats per speaker and aggregate at the end
         #       instead of aggregating per batch
 
-        # Compute WER scores for the predictions
-        prediction_error_metrics = Counter()
-        prediction_error_metrics = accumulatable_wer_stats(
-            refs=true_canonical_phones,
-            hyps=pred_canonical_phones,
-            stats=prediction_error_metrics,
+        # Compute WER scores for the canonical label reconstruction
+        # This is an ASR task, so we use the WER metric to compute the phoneme error rate
+        asr_metrics = self.compute_asr_metrics(
+            true_canonical_phones=true_canonical_phones,
+            pred_canonical_phones=pred_canonical_phones,
         )
-
-        # Phoneme error rate
-        records["per"].append(prediction_error_metrics["WER"])
+        records["per"].append(asr_metrics["WER"])
 
         # TODO: refer to the paper we took this approach from
+        # https://arxiv.org/pdf/2203.15937.pdf 
 
-        # Metric for mispronunciation deection: f1-score
-        # Way of computing it:
-        # 1. Aligning the true and predicted canonical sequences
-        # 2. Using the true perceived sequences to find the errors:
-        #    - true positives: neither the true or the predicted canonical phone agree with the true perceived phone
-        #    - true negatives: both the true and the predicted canonical phone agree with the true perceived phone
-        #    - false positives: the true canonical and true perceived phones are the same, but the predicted canonical phone differs
-        #    - false negatives: the predicted canonical and true perceived phones are the same, but the true canonical phone differs
-
-        prediction_stats = wer_details_for_batch(
-            ids=range(len(true_canonical_phones)),
-            refs=true_canonical_phones,
-            hyps=pred_canonical_phones,
-            compute_alignments=True,
-        )
-
-        empty_token = "<eps>"
-        for prediction_stat, perceived_utterance in zip(
-            prediction_stats, true_perceived_phones
+        for true_canonical_1batch, true_perceived_1batch, pred_canonical_1batch in zip(
+            true_canonical_phones, true_perceived_phones, pred_canonical_phones
         ):
-            # We assume that there is no need for aligning the L1 and L2 labels,
-            # because the minimum edit distance between them can be achieved using
-            # only substitutions and matches. This holds for all data points,
-            # except data point #2006 in our dataset.
-            # Data point #2006 has an edit distance of 10 instead of 9 if using this method.
-            # We choose to use this simplification and potentially remove data point #2006 in
-            # the future.
-            pred_to_true_canonical_alignment = prediction_stat["alignment"]
-            canonical_utterance = prediction_stat["ref_tokens"]
-            pred_canonical_utterance = prediction_stat["hyp_tokens"]
+            mdd_metrics = self.compute_mdd_metrics(
+                true_canonical_phones=true_canonical_1batch,
+                true_perceived_phones=true_perceived_1batch,
+                pred_canonical_phones=pred_canonical_1batch,
+            )
 
-            tp = 0
-            fp = 0
-            tn = 0
-            fn = 0
-
-            for op, true_idx, pred_idx in pred_to_true_canonical_alignment:
-                perceived_phone = (
-                    perceived_utterance[true_idx]
-                    if true_idx is not None
-                    else empty_token
-                )
-                canonincal_phone = (
-                    canonical_utterance[true_idx]
-                    if true_idx is not None
-                    else empty_token
-                )
-                pred_canonincal_phone = (
-                    pred_canonical_utterance[pred_idx]
-                    if pred_idx is not None
-                    else empty_token
-                )
-                if op == "=":  # pred_canonical == true_canonical
-                    if perceived_phone == canonincal_phone:
-                        tn += 1
-                    else:
-                        tp += 1
-                elif op == "S":  # substitution
-                    # This still detects mispronunciation
-                    if (
-                        canonincal_phone != perceived_phone
-                        and pred_canonincal_phone != perceived_phone
-                    ):
-                        tp += 1
-                    # We fail to detect mispronuncaition
-                    elif (
-                        canonincal_phone != perceived_phone
-                        and pred_canonincal_phone == perceived_phone
-                    ):
-                        fn += 1
-                    # We incorrectly detect mispronuncation
-                    elif (
-                        canonincal_phone == perceived_phone
-                        and pred_canonincal_phone != perceived_phone
-                    ):
-                        fp += 1
-                    else:
-                        assert False, "Incprrect substitution case"
-                elif op == "I":  # insertion
-                    assert true_idx == None, "Incorrect true index for insertion"
-                    # We think an insertion error occurred, but in reality it was a substitution error
-                    if canonincal_phone != perceived_phone:
-                        tp += 1
-                    # We think an insertion error occurred, but nothing did
-                    else:
-                        fp += 1
-                elif op == "D":  # deletion
-                    assert pred_idx == None, "Incorrect pred index for deletion"
-                    # We think a deletion error occurred, but in reality it was a substitution error
-                    if canonincal_phone != perceived_phone:
-                        tp += 1
-                    # We think a deletion error occurred, but nothing did
-                    else:
-                        fp += 1
-
-            precision = 0
-            if tp + fp > 0:
-                precision = tp / (tp + fp)
-            recall = 0
-            if tp + fn > 0:
-                recall = tp / (tp + fn)
-            f1_score = 0
-            if precision != 0 or recall != 0:
-                f1_score = 2 / ((1 / precision) + (1 / recall))
-
-            records["precision"].append(precision)
-            records["recall"].append(recall)
-            records["f1_score"].append(f1_score)
+            for k, v in mdd_metrics.items():
+                records[k].append(v)
+            # records["precision"].append(mdd_metrics["precision"])
+            # records["recall"].append(mdd_metrics["recall"])
+            # records["f1_score"].append(mdd_metrics["f1_score"])
 
         return loss
 
@@ -379,6 +544,8 @@ class DownstreamExpert(nn.Module):
         metrics = [
             "loss",
             "per",
+            "corr_diag_pct",
+            "err_diag_pct",
             "f1_score",
             "precision",
             "recall",
@@ -398,6 +565,7 @@ class DownstreamExpert(nn.Module):
 
         message_parts.append("\n")
         message = "|".join(message_parts)
+
         save_ckpt = []
         if avg_loss > self.best[prefix]:
             self.best[prefix] = avg_loss
