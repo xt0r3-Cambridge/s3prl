43a169266f56bb1299189abce5b928a1aaf522ab
Kornel Szabo <kgs37@cam.ac.uk>
2024-01-14 17:16:51 +0000

    Add wav2vec feature extractor

--- s3prl/downstream/mdd/model.py
+++ s3prl/downstream/mdd/model.py
@@ -21,16 +21,28 @@ from torch import Tensor
 # MODEL #
 #########
 class Model(nn.Module):
-    def __init__(self, input_dim, output_class_num, **kwargs):
+    def __init__(
+        self, input_dim, output_class_num, extractor_blocks, extractor_dim, **kwargs
+    ):
         super(Model, self).__init__()
 
+        if extractor_blocks > 0:
+            self.model = nn.Sequential(
+                nn.Linear(input_dim, extractor_dim),
+                *[
+                    nn.Linear(extractor_dim, extractor_dim)
+                    for _ in range(extractor_blocks - 1)
+                ],
+                nn.Linear(extractor_dim, output_class_num)
+            )
         # init attributes
-        self.linear = nn.Linear(input_dim, output_class_num)
+        else:
+            self.model = nn.Linear(input_dim, output_class_num)
 
     @jaxtyped
     @typeguard.typechecked
     def forward(
         self, features: Float[Tensor, "batch seq_len upstream_class_num"]
     ) -> Float[Tensor, "batch seq_len downstream_class_num"]:
-        preds = self.linear(features)
+        preds = self.model(features)
         return preds
